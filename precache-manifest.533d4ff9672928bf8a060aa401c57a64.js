b4ee9cb8bed038b6aa8bb3e7f7efa1ac407b5a6c
alestori7@gmail.com <alestori7@gmail.com>
2021-10-04 18:31:41 -0300

    Updates

---
+++ precache-manifest.533d4ff9672928bf8a060aa401c57a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983acfb82c0239b0ee2bfc9b428a1410",
+    "url": "/project-RTL/index.html"
+  },
+  {
+    "revision": "f2b791804a9e72088fb5",
+    "url": "/project-RTL/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/project-RTL/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-RTL/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b791804a9e72088fb5",
+    "url": "/project-RTL/static/js/main.adc7b535.chunk.js"
+  },
+  {
+    "revision": "89712c768770290ef0f3",
+    "url": "/project-RTL/static/js/runtime-main.d99e24ca.js"
+  }
+]);
